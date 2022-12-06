a1177a74c2aa35603ddd4008f880eec773197cbe
Paul Heasley <paul@eql.com>
2022-12-06 22:06:51 +1100

    Day 4

--- day4/day4.py
+++ day4/day4.py
@@ -2,15 +2,38 @@ import sys
 
 
 def range_contains(a, b):
-    pass
+    a_start, a_end = (int(i) for i in a.split("-"))
+    b_start, b_end = (int(i) for i in b.split("-"))
+    return (a_start <= b_start and a_end >= b_end) or (
+        a_start >= b_start and a_end <= b_end
+    )
+
+
+def range_overlaps(a, b):
+    a_start, a_end = (int(i) for i in a.split("-"))
+    b_start, b_end = (int(i) for i in b.split("-"))
+    return (
+        (a_start <= b_start <= a_end)
+        or (a_start <= b_end <= a_end)
+        or (b_start <= a_start <= b_end)
+        or (b_start <= a_end <= b_end)
+    )
+
+
+def count_contained(lines):
+    return sum(range_contains(*line.split(",")) for line in lines)
+
+
+def count_overlaps(lines):
+    return sum(range_overlaps(*line.split(",")) for line in lines)
 
 
 def main():
     filename = sys.argv[1]
     with open(filename) as f:
         lines = f.read().splitlines()
-    # result = badge_priority_sum(lines)
-    # print(result)
+    result = count_overlaps(lines)
+    print(result)
 
 
 if __name__ == "__main__":
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -5,7 +5,47 @@ import day4
 class TestDay4(unittest.TestCase):
     def test_range_contains(self):
         self.assertFalse(day4.range_contains("2-4", "6-8"))
+        self.assertFalse(day4.range_contains("5-7", "7-9"))
         self.assertTrue(day4.range_contains("2-8", "3-7"))
+        self.assertTrue(day4.range_contains("6-6", "4-6"))
+        self.assertTrue(day4.range_contains("59-59", "49-73"))
+        self.assertTrue(day4.range_contains("49-73", "59-59"))
+        self.assertTrue(day4.range_contains("1-1", "1-1"))
+
+    def test_range_overlaps(self):
+        self.assertFalse(day4.range_overlaps("2-4", "6-8"))
+        self.assertFalse(day4.range_overlaps("8-8", "2-2"))
+        self.assertTrue(day4.range_overlaps("5-7", "7-9"))
+        self.assertTrue(day4.range_overlaps("7-9", "5-7"))
+        self.assertTrue(day4.range_overlaps("2-8", "3-7"))
+        self.assertTrue(day4.range_overlaps("6-6", "4-6"))
+        self.assertTrue(day4.range_overlaps("59-59", "49-73"))
+        self.assertTrue(day4.range_overlaps("49-73", "59-59"))
+        self.assertTrue(day4.range_overlaps("1-1", "1-1"))
+        self.assertTrue(day4.range_overlaps("1-3", "2-4"))
+        self.assertTrue(day4.range_overlaps("2-4", "1-3"))
+
+    def test_count_contained(self):
+        lines = [
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8",
+        ]
+        self.assertEqual(day4.count_contained(lines), 2)
+
+    def test_count_overlaps(self):
+        lines = [
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8",
+        ]
+        self.assertEqual(day4.count_overlaps(lines), 4)
 
 
 if __name__ == "__main__":
