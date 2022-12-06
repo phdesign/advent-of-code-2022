f58fe52c508a477a1c2678d1e79a0377af843f3d
Paul Heasley <paul@eql.com>
2022-12-07 09:41:52 +1100

    Day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,21 @@
 import sys
 import re
+from operator import itemgetter
+
+
+def get_moves(lines):
+    return [
+        {
+            "count": int(m.group(1)),
+            "from": int(m.group(2)),
+            "to": int(m.group(3)),
+        }
+        for m in (
+            re.search(r"^move (\d+) from (\d+) to (\d+)", line)
+            for line in lines
+        )
+        if m
+    ]
 
 
 def get_stacks(lines):
@@ -14,12 +30,31 @@ def get_stacks(lines):
     ]
 
 
+def reorder_stacks(stacks, moves):
+    for move in moves:
+        for _ in range(move["count"]):
+            stacks[move["to"] - 1].append(stacks[move["from"] - 1].pop())
+
+
+def reorder_stacks_9001(stacks, moves):
+    for move in moves:
+        count, src, dest = itemgetter("count", "from", "to")(move)
+        stacks[dest - 1].extend(stacks[src - 1][-count:])
+        stacks[src - 1] = stacks[src - 1][: len(stacks[src - 1]) - count]
+
+
+def top_crates(input, reorder):
+    stack_string, move_string = input.split("\n\n")
+    stacks = get_stacks(stack_string.splitlines())
+    moves = get_moves(move_string.splitlines())
+    reorder(stacks, moves)
+    return "".join([s[-1] for s in stacks])
+
+
 def main():
     filename = sys.argv[1]
     with open(filename) as f:
-        lines = f.read().splitlines()
-    # result = count_overlaps(lines)
-    # print(result)
+        print(top_crates(f.read(), reorder_stacks_9001))
 
 
 if __name__ == "__main__":
--- day5/test_day5.py
+++ day5/test_day5.py
@@ -1,5 +1,11 @@
 import unittest
-from day5 import get_stacks
+from day5 import (
+    get_stacks,
+    get_moves,
+    reorder_stacks,
+    reorder_stacks_9001,
+    top_crates,
+)
 
 
 class TestSuite(unittest.TestCase):
@@ -14,6 +20,75 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(stacks[0], ["Z", "N"])
         self.assertEqual(stacks[1], ["M", "C", "D"])
         self.assertEqual(stacks[2], ["P"])
+
+    def test_get_moves(self):
+        input = """
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+        moves = get_moves(input.splitlines())
+        self.assertEqual(moves[0], {"count": 1, "from": 2, "to": 1})
+        self.assertEqual(moves[1], {"count": 3, "from": 1, "to": 3})
+        self.assertEqual(moves[2], {"count": 2, "from": 2, "to": 1})
+        self.assertEqual(moves[3], {"count": 1, "from": 1, "to": 2})
+
+    def test_reorder_stacks(self):
+        stacks = [["Z", "N"], ["M", "C", "D"], ["P"]]
+        moves = [{"count": 1, "from": 2, "to": 1}]
+        reorder_stacks(stacks, moves)
+        self.assertEqual(stacks, [["Z", "N", "D"], ["M", "C"], ["P"]])
+
+        moves = [{"count": 3, "from": 1, "to": 3}]
+        reorder_stacks(stacks, moves)
+        self.assertEqual(stacks, [[], ["M", "C"], ["P", "D", "N", "Z"]])
+
+        moves = [{"count": 2, "from": 2, "to": 1}]
+        reorder_stacks(stacks, moves)
+        self.assertEqual(stacks, [["C", "M"], [], ["P", "D", "N", "Z"]])
+
+        moves = [{"count": 1, "from": 1, "to": 2}]
+        reorder_stacks(stacks, moves)
+        self.assertEqual(stacks, [["C"], ["M"], ["P", "D", "N", "Z"]])
+
+    def test_reorder_stacks_9001(self):
+        stacks = [["Z", "N"], ["M", "C", "D"], ["P"]]
+        moves = [{"count": 1, "from": 2, "to": 1}]
+        reorder_stacks_9001(stacks, moves)
+        self.assertEqual(stacks, [["Z", "N", "D"], ["M", "C"], ["P"]])
+
+        moves = [{"count": 3, "from": 1, "to": 3}]
+        reorder_stacks_9001(stacks, moves)
+        self.assertEqual(stacks, [[], ["M", "C"], ["P", "Z", "N", "D"]])
+
+    def test_top_crates(self):
+        input = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+        self.assertEqual(top_crates(input, reorder_stacks), "CMZ")
+
+    def test_top_crates_9001(self):
+        input = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+        self.assertEqual(top_crates(input, reorder_stacks_9001), "MCD")
 
 
 if __name__ == "__main__":
